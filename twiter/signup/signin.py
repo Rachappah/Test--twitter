1bd6535b2d3070b0e9d84ab070002ae6e8e7d866
Rachappah <Rachappa@HP>
2017-03-30 12:50:36 +0530

    New test script on twitter

---
+++ twiter/signup/signin.py
@@ -0,0 +1,15 @@
+'''
+Created on 30/03/2017
+
+@author: Rachappa
+'''
+from selenium import webdriver
+import time
+from lib2to3.tests.support import driver
+if __name__ == '__main__':
+    driver=webdriver.Firefox()
+    driver.get("https://twitter.com/signup?lang=en")
+    driver.find_element_by_id("full-name").send_keys("rachappa")
+    driver.find_element_by_id("email").send_keys("rachappahalinge@gmail.com")
+    driver.find_element_by_id("password").send_keys("rach222")
+    
