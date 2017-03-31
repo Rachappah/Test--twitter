bae18228d5563df2f08db773e58aff97548eff1b
Rachappah <Rachappa@HP>
2017-03-31 12:22:55 +0530

    new script twitter

---
+++ twit/newtwit/check.py
@@ -0,0 +1,8 @@
+'''
+Created on 31/03/2017
+
+@author: Rachappa
+'''
+
+if __name__ == '__main__':
+    
--- twiter/signup/signin.py
+++ twiter/signup/signin.py
@@ -5,16 +5,13 @@ Created on 30/03/2017
 '''
 from selenium import webdriver
 import time
-from lib2to3.tests.support import driver
 if __name__ == '__main__':
     driver=webdriver.Firefox()
     driver.get("https://twitter.com/signup?lang=en")
     driver.find_element_by_id("full-name").send_keys("rachappa")
     driver.find_element_by_id("email").send_keys("rachappahalinge@gmail.com")
-    driver.find_element_by_id("password").send_keys("rach222")
-    element=driver.find_element_by_class_name("js-current-language")
-    e1=element.click()
-    driver.find_element_by_xpath("//(li[@role='presentation'])[7]/a[@class='js-language-link js-tooltip']").click()
+    driver.find_element_by_id("last_name")
+    driver.find_element_by_id()
     
     
     
